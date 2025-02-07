69c211b2025a7a8b893107fe1107c18bcfb3575e
Teo Camarasu <teofilcamarasu@gmail.com>
2025-02-07 10:56:08 +0000

    Replace symlinks with actual files

---
+++ opencv/doc/ExampleExtractor.hs
@@ -0,0 +1,363 @@
+{-# language QuasiQuotes #-}
+{-# language TemplateHaskell #-}
+{-# language CPP #-}
+
+module ExampleExtractor
+  ( Animation
+
+  , renderImage
+  , renderAnimation
+
+  , extractExampleImages
+  ) where
+
+import "base" Control.Arrow ( (***), (&&&) )
+import "base" Control.Monad
+import "base" Data.Bifunctor
+import "base" Data.Either
+import "base" Data.Maybe
+import "base" Data.Word
+import qualified "containers" Data.Map.Strict as M
+import "directory" System.Directory ( canonicalizePath )
+import qualified "haskell-src-exts" Language.Haskell.Exts.Extension as Hse
+import qualified "haskell-src-exts" Language.Haskell.Exts.Parser as Hse
+import qualified "haskell-src-exts" Language.Haskell.Exts.Syntax as Hse
+import qualified "haskell-src-exts" Language.Haskell.Exts.SrcLoc as Hse
+import qualified "Glob" System.FilePath.Glob as G
+import "JuicyPixels" Codec.Picture.ColorQuant as JP
+import "JuicyPixels" Codec.Picture.Gif        as JP
+import "JuicyPixels" Codec.Picture.Types      as JP
+import qualified "opencv" OpenCV as CV
+import qualified "opencv" OpenCV.Juicy as CVJ
+import qualified "text" Data.Text as T
+import qualified "text" Data.Text.Encoding as TE
+import qualified "bytestring" Data.ByteString as B
+import qualified "bytestring" Data.ByteString.Lazy as BL
+import "template-haskell" Language.Haskell.TH
+import "template-haskell" Language.Haskell.TH.Syntax
+import "this" Language.Haskell.Meta.Syntax.Translate ( toDecs )
+
+#if !MIN_VERSION_base(4,11,0)
+import "base" Data.Monoid
+#endif
+
+--------------------------------------------------------------------------------
+
+-- An animation is a list of images. Each image has a duration
+-- specified in hundreths of a second.
+type Animation shape channels depth = [(Int, CV.Mat shape channels depth)]
+
+--------------------------------------------------------------------------------
+
+renderImage
+    :: FilePath
+    -> CV.Mat ('CV.S [height, width]) channels depth
+    -> IO ()
+renderImage fp img = do
+    let bs = CV.exceptError $ CV.imencode (CV.OutputPng CV.defaultPngParams) img
+    putStr $ "Writing image " <> dest <> " ..."
+    B.writeFile dest bs
+    putStrLn " OK"
+  where
+    dest = mkDestPath fp
+
+renderAnimation
+    :: FilePath
+    -> Animation ('CV.S [height, width]) ('CV.S 3) ('CV.S Word8)
+    -> IO ()
+renderAnimation fp imgs = do
+    putStr $ "Writing animation " <> dest <> " ..."
+    case gif of
+      Left errMsg -> putStrLn $ " " <> errMsg
+      Right bs -> BL.writeFile dest bs
+    putStrLn " OK"
+  where
+    gif :: Either String BL.ByteString
+    gif = JP.encodeGifImages JP.LoopingForever palImgs
+
+    palImgs :: [(JP.Palette, JP.GifDelay, JP.Image JP.Pixel8)]
+    palImgs =
+        map (\(delay, img) ->
+              let (img8, pal) = JP.palettize JP.defaultPaletteOptions img
+              in (pal, delay, img8)
+            )
+            jpImgs
+
+    jpImgs :: [(JP.GifDelay, JP.Image JP.PixelRGB8)]
+    jpImgs = map (second CVJ.toImage) imgs
+
+    dest = mkDestPath fp
+
+mkDestPath :: FilePath -> FilePath
+mkDestPath fp = "doc/generated/" <> fp
+
+--------------------------------------------------------------------------------
+
+data SrcLoc
+   = SrcLoc
+     { locFile :: !FilePath
+     , locLine :: !Int
+     }
+
+-- | Haskell source code containing 0, 1 or more examples.
+data ExampleSrc
+   = ExampleSrc
+     { exsLoc :: !SrcLoc
+     , exsSrc :: !T.Text
+     }
+
+data ParsedExampleSrc
+   = ParsedExampleSrc
+     { pexsLoc   :: !SrcLoc
+     , pexsDecls :: ![Dec]
+     }
+
+-- | A single line of Haskell source code.
+data SrcLine
+   = SrcLine
+     { srcLoc  :: !SrcLoc
+     , srcLine :: !T.Text
+     }
+
+data SymbolType
+   = SymImage
+   | SymImageAction
+     deriving (Show, Eq)
+
+data ExampleProps
+   = ExampleProps
+     { exPropIO        :: !Bool
+     , exPropAnimation :: !Bool
+     } deriving Show
+
+data RenderTarget
+   = RenderTarget
+     { rtDestination :: !FilePath
+       -- ^ Relative path where the symbol must be rendered as an image file.
+     , rtSymbolName :: !Name
+       -- ^ Name of a top level symbol (function or CAF) that is either an image
+       -- or an IO action that yields an image.
+     , rtSymbolProps :: !ExampleProps
+     } deriving Show
+
+--------------------------------------------------------------------------------
+
+extractExampleImages :: FilePath -> Q [Dec]
+extractExampleImages srcDir = do
+    haskellPaths <- runIO $ findHaskellPaths srcDir
+    mapM_ (addDependentFile <=< runIO . canonicalizePath) haskellPaths
+
+    ((exampleSrcs, renderTargets) :: ([ExampleSrc], [RenderTarget])) <- runIO $ do
+      xs <- mapM findExamples haskellPaths
+      pure $ (concat *** concat) $ unzip xs
+
+    let parseErrors       :: [String]
+        parsedExampleSrcs :: [ParsedExampleSrc]
+        (parseErrors, parsedExampleSrcs) = partitionEithers $ map parseExampleSrc exampleSrcs
+
+        examplesTH :: [Dec]
+        examplesTH = concatMap (\pexs -> parsedExampleLinePragma pexs : pexsDecls pexs)
+                               parsedExampleSrcs
+
+        exampleTypes :: M.Map Name Type
+        exampleTypes = M.fromList $ mapMaybe asSigD examplesTH
+
+        renderTargets' :: [RenderTarget]
+        renderTargets' =
+            mapMaybe
+              (\rt -> do
+                 exampleType <- M.lookup (rtSymbolName rt) exampleTypes
+                 pure rt {rtSymbolProps = classifyExample exampleType}
+              )
+              renderTargets
+
+    unless (null parseErrors) $
+      error $ show parseErrors
+
+    mdecs <- mkRenderExampleImages renderTargets'
+    pure $ examplesTH <> mdecs
+
+parsedExampleLinePragma :: ParsedExampleSrc -> Dec
+parsedExampleLinePragma pexs =
+    PragmaD $ LineP (locLine loc) (locFile loc)
+  where
+    loc = pexsLoc pexs
+
+parseExampleSrc :: ExampleSrc -> Either String ParsedExampleSrc
+parseExampleSrc exs =
+    case parseDecsHse (locFile $ exsLoc exs) $ T.unpack $ haddockToHaskell $ exsSrc exs of
+      Left errMsg -> Left $ (locFile $ exsLoc exs) <> ": " <> errMsg
+      Right decls -> Right
+                     ParsedExampleSrc
+                     { pexsLoc   = exsLoc exs
+                     , pexsDecls = toDecs decls
+                     }
+
+
+asSigD :: Dec -> Maybe (Name, Type)
+asSigD (SigD n t) = Just (n, t)
+asSigD _ = Nothing
+
+-- Really hacky way of determining the properties of an example based
+-- on its type.
+classifyExample :: Type -> ExampleProps
+classifyExample (ForallT _ _ t) = classifyExample t
+classifyExample (AppT (ConT n) t2) | nameBase n == nameBase ''IO = checkIOAnimation t2
+classifyExample (AppT t1 _)     = classifyExample t1
+classifyExample (VarT _)        = ExampleProps False False
+classifyExample (ConT n) | nameBase n == nameBase ''Animation = ExampleProps False True
+classifyExample (PromotedT _)   = ExampleProps False False
+classifyExample _               = ExampleProps False False
+
+checkIOAnimation :: Type -> ExampleProps
+checkIOAnimation (ForallT _ _ t) = checkIOAnimation t
+checkIOAnimation (AppT t1 _)     = checkIOAnimation t1
+checkIOAnimation (VarT _)        = ExampleProps True False
+checkIOAnimation (ConT n) | nameBase n == nameBase ''Animation = ExampleProps True True
+checkIOAnimation (PromotedT _)   = ExampleProps True False
+checkIOAnimation _               = ExampleProps True False
+
+parseDecsHse :: String -> String -> Either String [Hse.Decl Hse.SrcSpanInfo]
+parseDecsHse fileName str =
+    case Hse.parseModuleWithMode (parseMode fileName) str of
+      Hse.ParseFailed _srcLoc err -> Left err
+      Hse.ParseOk (Hse.Module _ _ _ _  decls) -> Right decls
+      Hse.ParseOk _ -> Left "Invalid module"
+
+parseMode :: String -> Hse.ParseMode
+parseMode fileName =
+    Hse.ParseMode
+    { Hse.parseFilename         = fileName
+    , Hse.baseLanguage          = Hse.Haskell2010
+    , Hse.extensions            = map Hse.EnableExtension exts
+    , Hse.ignoreLanguagePragmas = False
+    , Hse.ignoreLinePragmas     = False
+    , Hse.fixities              = Nothing
+    , Hse.ignoreFunctionArity   = False
+    }
+  where
+    exts :: [Hse.KnownExtension]
+    exts =
+      [ Hse.BangPatterns
+      , Hse.DataKinds
+      , Hse.FlexibleContexts
+      , Hse.LambdaCase
+      , Hse.OverloadedStrings
+      , Hse.PackageImports
+      , Hse.PolyKinds
+      , Hse.ScopedTypeVariables
+      , Hse.TupleSections
+      , Hse.TypeFamilies
+      , Hse.TypeOperators
+      , Hse.PostfixOperators
+      , Hse.QuasiQuotes
+      , Hse.UnicodeSyntax
+      , Hse.MagicHash
+      , Hse.PatternSignatures
+      , Hse.MultiParamTypeClasses
+      , Hse.RankNTypes
+      ]
+
+-- | Generate code for every render target
+--
+-- Executing the generated code will actually render the target.
+mkRenderExampleImages :: [RenderTarget] -> Q [Dec]
+mkRenderExampleImages renderTargets = [d|
+    renderExampleImages :: IO ()
+    renderExampleImages = $(pure doRender)
+    |]
+  where
+    doRender :: Exp
+    doRender =
+        DoE $ do
+          rt <- renderTargets
+          let sym = VarE $ rtSymbolName rt
+              fp  = LitE $ StringL $ "examples/" <> rtDestination rt
+              props = rtSymbolProps rt
+              render | exPropAnimation props = 'renderAnimation
+                     | otherwise             = 'renderImage
+          pure $ NoBindS $
+            if exPropIO props
+            then VarE '(>>=) `AppE` sym `AppE` (VarE render `AppE` fp)
+            else VarE render `AppE` fp `AppE` sym
+
+#if MIN_VERSION_Glob(0,9,0)
+findHaskellPaths :: FilePath -> IO [FilePath]
+findHaskellPaths = fmap concat . G.globDir [G.compile "**/*.hs", G.compile "**/*.hsc"]
+#else
+findHaskellPaths srcDir = do
+  (paths, _) <- G.globDir [G.compile "**/*.hs", G.compile "**/*.hsc"] srcDir
+  pure $ concat paths
+#endif
+
+haddockToHaskell :: T.Text -> T.Text
+haddockToHaskell =
+    T.replace "\\\\" "\\"
+  . T.replace "\\`" "`"
+  . T.replace "\\<" "<"
+  . T.replace "\\/" "/"
+
+findExamples :: FilePath -> IO ([ExampleSrc], [RenderTarget])
+findExamples fp = ((parseExamples &&& parseGeneratedImages) . textToSource fp) <$> readFileUtf8 fp
+
+-- https://www.snoyman.com/blog/2016/12/beware-of-readfile
+-- Data.Text.readFile has problems with character encoding.
+readFileUtf8 :: FilePath -> IO T.Text
+readFileUtf8 = fmap TE.decodeUtf8 . B.readFile
+
+textToSource :: FilePath -> T.Text -> [SrcLine]
+textToSource fp txt = zipWith lineToSource [1..] (T.lines txt)
+  where
+    lineToSource :: Int -> T.Text -> SrcLine
+    lineToSource n line =
+        SrcLine
+        { srcLoc  = SrcLoc {locFile = fp, locLine = n}
+        , srcLine = line
+        }
+
+parseExamples :: [SrcLine] -> [ExampleSrc]
+parseExamples = findStart
+  where
+    findStart :: [SrcLine] -> [ExampleSrc]
+    findStart      []  = []
+    findStart   (_:[]) = []
+    findStart (_:_:[]) = []
+    findStart (a:b:c:ls)
+              | srcLine a == "Example:"
+              , srcLine b == ""
+              , srcLine c == "@"
+                     = findEnd [] ls
+    findStart (_:ls) = findStart ls
+
+    findEnd :: [SrcLine] -> [SrcLine] -> [ExampleSrc]
+    findEnd _acc [] = []
+    findEnd acc (l:ls)
+        | srcLine l == "@" =
+            case reverse acc of
+              []    -> findStart ls
+              revAcc@(firstLine:_) ->
+                  let exs = ExampleSrc
+                            { exsLoc = srcLoc firstLine
+                            , exsSrc = T.unlines (map srcLine revAcc)
+                            }
+                  in exs : findStart ls
+        | otherwise = findEnd (l:acc) ls
+
+parseGeneratedImages :: [SrcLine] -> [RenderTarget]
+parseGeneratedImages = concatMap $ parseLine . srcLine
+  where
+    parseLine :: T.Text -> [RenderTarget]
+    parseLine line = maybeToList $ do
+      let fromPrefix = snd $ T.breakOn prefix line
+      rest <- T.stripPrefix prefix fromPrefix
+      case take 2 $ T.words rest of
+        [fp, funcName] ->
+            pure RenderTarget
+                 { rtDestination = T.unpack $ fp
+                 , rtSymbolName = mkName $ T.unpack $ fromMaybe funcName (T.stripSuffix ">>" funcName)
+                   -- Later on we will determine the actual properties.
+                 , rtSymbolProps = ExampleProps False False
+                 }
+        _ -> Nothing
+
+    prefix = "<<doc/generated/examples/"
---
+++ opencv/doc/images.hs
@@ -0,0 +1,280 @@
+{-# language TemplateHaskell #-}
+{-# language OverloadedStrings #-}
+{-# language FlexibleContexts #-}
+{-# language CPP #-}
+
+module Main where
+
+import "base" Data.Functor ( void )
+import "base" Data.Foldable ( for_ )
+import "base" Data.Int
+import "base" Data.Monoid ( (<>) )
+import "base" Data.Proxy
+import "base" Data.Traversable
+import "base" Data.Word
+import "base" Foreign.C.Types ( CFloat, CDouble )
+import "base" GHC.TypeLits
+import "base" System.IO.Unsafe ( unsafePerformIO )
+import "data-default" Data.Default
+import qualified "bytestring" Data.ByteString as B
+import qualified "JuicyPixels" Codec.Picture
+import qualified "linear" Linear.Metric as Linear ( norm )
+import "linear" Linear ( M33 )
+import "linear" Linear.Vector ( (^+^), (^-^) )
+import "linear" Linear.V2 ( V2(..) )
+import "linear" Linear.V3 ( V3(..) )
+import "linear" Linear.V4 ( V4(..) )
+import "opencv" OpenCV
+import qualified "opencv" OpenCV.Juicy
+import "opencv" OpenCV.Unsafe
+#ifdef HAVE_OPENCV_EXTRA
+import "opencv-extra" OpenCV.Extra
+#endif
+import "primitive" Control.Monad.Primitive ( PrimMonad, PrimState )
+import qualified "text" Data.Text as T
+import qualified "vector" Data.Vector as V
+import qualified "vector" Data.Vector.Storable as VS
+import "transformers" Control.Monad.Trans.Class ( lift )
+
+import "this" ExampleExtractor
+
+--------------------------------------------------------------------------------
+
+transparent, white, black, blue, green, red :: Scalar
+transparent = toScalar (V4 255 255 255   0 :: V4 Double)
+white       = toScalar (V4 255 255 255 255 :: V4 Double)
+black       = toScalar (V4   0   0   0 255 :: V4 Double)
+blue        = toScalar (V4 255   0   0 255 :: V4 Double)
+green       = toScalar (V4   0 255   0 255 :: V4 Double)
+red         = toScalar (V4   0   0 255 255 :: V4 Double)
+
+type Kodak_768x512    = Mat (ShapeT [ 512,  768]) ('S 3) ('S Word8)
+type Kodak_512x341    = Mat (ShapeT [ 341,  512]) ('S 3) ('S Word8)
+type Frog             = Mat (ShapeT [ 390,  500]) ('S 3) ('S Word8)
+type Lambda           = Mat (ShapeT [ 256,  256]) ('S 1) ('S Word8)
+type Circles_1000x625 = Mat (ShapeT [ 625, 1000]) ('S 3) ('S Word8)
+type Building_868x600 = Mat (ShapeT [ 600,  868]) ('S 3) ('S Word8)
+type DamageMask       = Mat (ShapeT [ 341,  512]) ('S 1) ('S Word8)
+type Lenna_512x512    = Mat (ShapeT [ 512,  512]) ('S 3) ('S Word8)
+type Arnold           = Mat (ShapeT [3504, 2336]) ('S 3) ('S Word8)
+type Arnold_small     = Mat (ShapeT [ 900,  600]) ('S 3) ('S Word8)
+type Hand             = Mat (ShapeT [ 543,  400]) ('S 3) ('S Word8)
+type Compass          = Mat (ShapeT [ 497,  500]) ('S 3) ('S Word8)
+type CodexLeicester   = Mat (ShapeT [ 416,  500]) ('S 3) ('S Word8)
+
+birds_768x512 :: Kodak_768x512
+birds_768x512 = exceptError $ coerceMat $ unsafePerformIO $
+                  imdecode ImreadUnchanged <$> B.readFile "data/kodim23.png"
+
+flower_768x512 :: Kodak_768x512
+flower_768x512 =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/kodim07.png"
+
+sailboat_768x512 :: Kodak_768x512
+sailboat_768x512 =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/kodim06.png"
+
+bikes_768x512 :: Kodak_768x512
+bikes_768x512 =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/kodim05.png"
+
+barn_768x512 :: Kodak_768x512
+barn_768x512 =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/kodim22.png"
+
+damageMask :: DamageMask
+damageMask =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/damage_mask.png"
+
+smallerKodakImg :: Kodak_768x512 -> Kodak_512x341
+smallerKodakImg img =
+    exceptError $ coerceMat =<<
+      resize (ResizeAbs $ toSize (V2 512 341 :: V2 Int32))
+             InterArea
+             img
+
+birds_512x341 :: Kodak_512x341
+birds_512x341 = smallerKodakImg birds_768x512
+
+flower_512x341 :: Kodak_512x341
+flower_512x341 = smallerKodakImg flower_768x512
+
+sailboat_512x341 :: Kodak_512x341
+sailboat_512x341 = smallerKodakImg sailboat_768x512
+
+barn_512x341 :: Kodak_512x341
+barn_512x341 = smallerKodakImg barn_768x512
+
+bikes_512x341 :: Kodak_512x341
+bikes_512x341 = smallerKodakImg bikes_768x512
+
+frog :: Frog
+frog =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/kikker.jpg"
+
+lambda :: Lambda
+lambda =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadGrayscale <$> B.readFile "data/lambda.png"
+
+circles_1000x625 :: Circles_1000x625
+circles_1000x625 =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/circles.png"
+
+building_868x600 :: Building_868x600
+building_868x600 =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/building.jpg"
+
+lenna_512x512 :: Lenna_512x512
+lenna_512x512 =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/Lenna.png"
+
+arnold :: Arnold
+arnold =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/arnold-schwarzenegger.jpg"
+
+arnold_small :: Arnold_small
+arnold_small =
+    exceptError $ coerceMat =<<
+      resize (ResizeAbs $ toSize (V2 600 900 :: V2 Int32))
+             InterArea
+             arnold
+
+hand :: Hand
+hand =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/hand.jpg"
+
+compass :: Compass
+compass =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/compass.jpg"
+
+codexLeicester :: CodexLeicester
+codexLeicester =
+    exceptError $ coerceMat $ unsafePerformIO $
+      imdecode ImreadUnchanged <$> B.readFile "data/codex_leicester.jpg"
+
+--------------------------------------------------------------------------------
+
+type CarOverhead = Animation (ShapeT [240, 320]) ('S 3) ('S Word8)
+
+carOverhead :: CarOverhead
+carOverhead = unsafePerformIO $ loadAnimation 4 "data/car-overhead-3.mp4"
+
+--------------------------------------------------------------------------------
+
+loadAnimation
+    :: (ToNatDS (Proxy h), ToNatDS (Proxy w))
+    => Int
+    -> FilePath
+    -> IO (Animation ('S [h, w]) ('S 3) ('S Word8))
+loadAnimation delay fp = do
+    cap <- newVideoCapture
+    exceptErrorIO $ videoCaptureOpen cap (VideoFileSource fp Nothing)
+    frames <- grabFrames cap []
+    exceptErrorIO $ videoCaptureRelease cap
+    pure $ map (delay, ) frames
+  where
+    grabFrames cap frames = do
+        ok <- videoCaptureGrab cap
+        if ok
+        then do
+          mbFrameRaw <- videoCaptureRetrieve cap
+          case mbFrameRaw of
+            Nothing -> pure $ reverse frames
+            Just frameRaw -> do
+              frame <- exceptErrorIO $ coerceMat frameRaw
+              grabFrames cap (frame : frames)
+        else pure $ reverse frames
+
+--------------------------------------------------------------------------------
+
+-- We use some padding around the small image in which we draw the
+-- lines. This is because antialiasing doesn't seem to work near the
+-- edges of an image.
+lineTypeImg :: LineType -> Mat ('S ['D, 'D]) ('S 4) ('S Word8)
+lineTypeImg lineType = exceptError $ do
+    img <- withMatM (h + 2 * p ::: w + 2 * p ::: Z)
+                    (Proxy :: Proxy 4)
+                    (Proxy :: Proxy Word8)
+                    transparent $ \imgM -> do
+             lift $ line imgM (pure p + V2 0 h) (pure p + V2 w 0) black 1 lineType 0
+    resize (ResizeRel $ pure zoom) InterNearest
+           =<< matSubRect img (toRect $ HRect (pure p) (V2 w h))
+  where
+    w, h, p :: Int32
+    w = h * 3
+    h = 5
+    p = 20
+    zoom = 8
+
+fontImg :: Font -> Mat ('S ['D, 'D]) ('S 4) ('S Word8)
+fontImg font = exceptError $
+    withMatM (th * 3 ::: tw ::: Z)
+             (Proxy :: Proxy 4)
+             (Proxy :: Proxy Word8)
+             transparent $ \imgM -> do
+      putText
+        imgM
+        txt
+        (V2 0 (th * 2 - baseLine) :: V2 Int32)
+        font
+        black
+        thickness
+        LineType_AA False
+  where
+    txt = "The quick brown fox jumps over the lazy dog"
+    (size2i, baseLine) = getTextSize txt font thickness
+    tw, th :: Int32
+    V2 tw th = fromSize size2i
+    thickness = 1
+
+vennCircleA
+    :: (PrimMonad m, ToScalar color)
+    => Mut (Mat ('S [height, width]) channels depth) (PrimState m)
+    -> color
+    -> Int32
+    -> m ()
+vennCircleA imgM color thickness =
+    circle imgM (V2 100 100 :: V2 Int32) 90 color thickness LineType_AA 0
+
+vennCircleB
+    :: (PrimMonad m, ToScalar color)
+    => Mut (Mat ('S [height, width]) channels depth) (PrimState m)
+    -> color
+    -> Int32
+    -> m ()
+vennCircleB imgM color thickness =
+    circle imgM (V2 220 100 :: V2 Int32) 90 color thickness LineType_AA 0
+
+--------------------------------------------------------------------------------
+
+extractExampleImages "src"
+
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+    renderExampleImages
+    renderImage "birds_512x341.png"    birds_512x341
+    renderImage "flower_512x341.png"   flower_512x341
+    renderImage "sailboat_512x341.png" sailboat_512x341
+    renderImage "bikes_512x341.png"    bikes_512x341
+    for_ [minBound .. maxBound] $ \lineType ->
+      renderImage (show lineType <> ".png") (lineTypeImg lineType)
+    for_ [minBound .. maxBound] $ \fontFace -> do
+      renderImage (show fontFace <> ".png")         (fontImg $ Font fontFace NotSlanted 1)
+      renderImage (show fontFace <> "_slanted.png") (fontImg $ Font fontFace Slanted    1)
+
+--------------------------------------------------------------------------------
